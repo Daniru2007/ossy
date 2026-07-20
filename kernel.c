dc49179a485a5d3f9c14524c39d02628a2d26a4e
Daniru2007 <daniruidk@gmail.com>
2026-07-20 16:26:57 +0530

    Exception check good

--- kernel.c
+++ kernel.c
@@ -12,4 +12,8 @@ void kernel_main(void) {
         vga[i*2]     = str[i];
         vga[i*2 + 1] = 0x07; // light grey on black
     }
+    volatile int a = 10;
+    volatile int b = 0;
+    volatile int c = a / b;
+    (void)c;
 }
